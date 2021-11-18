39f5fc709df2d93e69e9504737f49c00828ff15f
anshul-retispec <anshul@retispec.com>
2021-11-18 15:04:29 -0500

    update

--- RetiCare/lib/models/Questions.dart
+++ RetiCare/lib/models/Questions.dart
@@ -12,49 +12,49 @@ const List sample_data = [
     "question":
         "What year is it ?",
     "options": ['2022', '2021', '2020', '2019'],
-    "answer_index": 2,
+    "answer_index": 1,
   },
   {
     "id": 2,
     "question": "What season is it ?",
     "options": ['Fall', 'Winter', 'Spring', 'Summer'],
-    "answer_index": 1,
+    "answer_index": 0,
   },
   {
     "id": 3,
     "question": "What month is it ?",
     "options": ['September', 'October', 'November', 'December'],
-    "answer_index": 3,
+    "answer_index": 2,
   },
   {
     "id": 4,
     "question": "What is today's date ?",
     "options": ['16', '18', '17', '19'],
-    "answer_index": 2,
+    "answer_index": 1,
   },
   {
     "id": 5,
     "question": "What day of the week is it ?",
     "options": ['Monday', 'Thursday', 'Friday', 'Saturday'],
-    "answer_index": 2,
+    "answer_index": 1,
   },
   {
     "id": 6,
     "question": "What country are we in ?",
     "options": ['Canada', 'USA', 'Mexico', 'Cuba'],
-    "answer_index": 1,
+    "answer_index": 0,
   },
   {
     "id": 7,
     "question": "What province are we in ?",
     "options": ['Ontario', 'British Columbia', 'Saskatchewan', 'Manitoba'],
-    "answer_index": 1,
+    "answer_index": 0,
   },
   {
     "id": 8,
     "question": "What city/town is it ?",
     "options": ['Toronto', 'Mississauga', 'Oakville', 'Scarborough'],
-    "answer_index": 2,
+    "answer_index": 0,
   },
 
 ];
--- RetiCare/lib/screens/welcome/welcome_screen.dart
+++ RetiCare/lib/screens/welcome/welcome_screen.dart
@@ -18,13 +18,29 @@ class WelcomeScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Spacer(flex: 2), //2/6
-                  Text(
-                    "Let's Play Quiz,",
-                    style: Theme.of(context).textTheme.headline4.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                  Row(
+                    children: [
+                      Text(
+                        "Age",
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Wise",
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.amber[900], fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        " MMSE,",
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
+                  Spacer(),// 1/6
                   Text("Enter your informations below"),
-                  Spacer(), // 1/6
+                  SizedBox(height:10),
+                  Text("Name"),
                   TextField(
                     decoration: InputDecoration(
                       filled: true,
@@ -35,7 +51,19 @@ class WelcomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Spacer(), // 1/6
+                  SizedBox(height:10),
+                  Text("Age"),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Age",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(),// 1/6
                   InkWell(
                     onTap: () => Get.to(QuizScreen()),
                     child: Container(
@@ -47,7 +75,7 @@ class WelcomeScreen extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(12)),
                       ),
                       child: Text(
-                        "Lets Start Quiz",
+                        "Start",
                         style: Theme.of(context)
                             .textTheme
                             .button
